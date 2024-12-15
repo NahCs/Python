fb640054ec2bdf1513e6f3fc13f4a76eb66f1b5e
NahCs <133619234+NahCs@users.noreply.github.com>
2024-12-15 08:32:45 -0300

    Update Ejecicio.py
    Se agregaron varias opciones mas: Actualizar producto, eliminar producto, buscar producto y bajo stock

--- TT/Menu_interactivo/Ejecicio.py
+++ TT/Menu_interactivo/Ejecicio.py
@@ -1,22 +1,36 @@
 inventario = []
 
-
-def agregar_producto():
-    nombre = input("Ingrese el nombre del producto: ")
-    cantidad = input("Ingrese la cantidad: ")
-    inventario.append({"nombre": nombre, "cantidad": cantidad})
-    print(f" '{nombre}' con cantidad {cantidad} fue agregado al inventario\n")
-
-
-def ver_inventario():
+def mostrar_productos():
     if inventario:
         print("Productos del inventario:")
-        for i, producto in enumerate(inventario, start=1):
-            print(f"{i}. {producto['nombre']} Cantidad: {producto['cantidad']} \n")
+        for producto in inventario:
+            print(f"ID: {producto['id']} | Nombre: {producto['nombre']} | Cantidad: {producto['cantidad']} | Precio: {producto['precio']} \n")
     else:
         print("<<<<<<<<<<<<<<<<<<<<<<<<<<")
         print("No hay prductos que mostar")
         print("<<<<<<<<<<<<<<<<<<<<<<<<<< \n")
+
+def registrar_producto():
+    id = int(input("ingrese el id del producto: "))
+    nombre = input("Ingrese el nombre del producto: ")
+    cantidad = int(input("Ingrese la cantidad: "))
+    precio = float(input("Ingrese el precio del producto: "))
+    inventario.append({"id": id, "nombre": nombre, "cantidad": cantidad, "precio": precio})
+    print(f" '{nombre}' con cantidad {cantidad} fue agregado al inventario con el numero de id: {id}\n")
+
+def actualizar_producto():
+    print("aca se podra actualizar los productos")
+
+
+def eliminar_producto():
+    print("aca se podran eleminar los productos")
+
+def buscar_producto():
+    print("Aca vas a poder buscar productos")
+
+def reporte_bajo_stock():
+    print("aca vas a poder quejarte de lo que hay")
+
 
 while True:
     print("<<<<<<<<<<<<<<<<<<<<<")
@@ -24,14 +38,26 @@ while True:
     print("<<<<<<<<<<<<<<<<<<<<< \n")
     print("1 Agregar producto")
     print("2 Ver inventario")
-    print("3 Salir")
+    print("3 Actualizar producto")
+    print("4 Eliminar producto")
+    print("5 Buscar producto")
+    print("6 ¿Bajo stock?")
+    print("7 Salir")
 
     opcion = input("Seleccione una opcion: ")
 
 
     if opcion == "1":
-        agregar_producto()
+        registrar_producto()
     elif opcion == "2":
-        ver_inventario()
+        mostrar_productos()
     elif opcion == "3":
+        actualizar_producto()
+    elif opcion == "4":
+        eliminar_producto()
+    elif opcion == "5":
+        buscar_producto()
+    elif opcion == "6":
+        reporte_bajo_stock()
+    elif opcion == "7":
         break
